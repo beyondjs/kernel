a509e12445d00fc9ee76f2390642403d95c48b5b
henrybox <ebox@socites.com>
2022-09-26 10:59:15 -0300

    HMR bug fix

--- core/beyond.ts
+++ core/beyond.ts
@@ -1,7 +1,6 @@
 import {languages} from "./languages";
 
 declare const bimport: (resource: string, version?: number) => Promise<any>;
-declare const breload: (resource: string, version?: number) => Promise<any>;
 
 export class Beyond {
     /**
@@ -22,18 +21,6 @@ export class Beyond {
      */
     async import(resource: string, version: number) {
         return await bimport(resource, version);
-    }
-
-    /**
-     * @deprecated
-     * Use breload instead of beyond.reload
-     *
-     * @param {string} resource
-     * @param {number} version
-     * @return {Promise<*>}
-     */
-    async reload(resource: string, version: number) {
-        return await breload(resource, version);
     }
 }
 
